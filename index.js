0fef9fd90c67a1734e67cd5f90c157f11702b973
lihe3 <lihe3@corp.beisencorp.com>
2017-10-17 14:30:32 +0800

    build:change entry

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-module.exports = require('./build/ReactGridLayout').default;
-module.exports.utils = require('./build/utils');
-module.exports.Responsive = require('./build/ResponsiveReactGridLayout').default;
-module.exports.Responsive.utils = require('./build/responsiveUtils');
-module.exports.WidthProvider = require('./build/components/WidthProvider').default;
+module.exports = require('./lib/ReactGridLayout').default;
+module.exports.utils = require('./lib/utils');
+module.exports.Responsive = require('./lib/ResponsiveReactGridLayout').default;
+module.exports.Responsive.utils = require('./lib/responsiveUtils');
+module.exports.WidthProvider = require('./lib/components/WidthProvider').default;
